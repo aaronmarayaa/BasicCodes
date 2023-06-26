ce52d380cd2761bc4dc282cc6602e13aa4a96cfe
Aaron Maraya <aaronmaraya7@gmail.com>
2023-06-26 18:07:03 +0800

    Fix code format
    -AngleChecker.css
    -AngleChecker.html
    -AngleChecker.js

--- Works/AngleChecker.js
+++ Works/AngleChecker.js
@@ -1,70 +1,23 @@
 function myFunction() {
-    var grade = Number(document.getElementById("result1").value);
-      
-    if(grade >= 1 && grade <= 89 ){
-      document.getElementById('textMargin_angles').innerHTML = "Acute angle";  
-      document.getElementById('angle').innerHTML = 
-      `<style>
-      #angle { 
-      height: 90px;
-      width: 100px;
-      border-bottom: 1px solid red; 
-      border-left: 2px solid red;
-      transform: skew(-45deg);
-      margin-top: 5%;
-      margin-left: 45%;
-      }  
-      </style>`;  
-      }
-  
-       else if(grade == 90){
-      document.getElementById('textMargin_angles').innerHTML = "Right angle"; 
-      document.getElementById('angle').innerHTML = 
-    `<style>
-      #angle{ 
-      height: 90px; 
-      width: 100px;
-      border-bottom: 1px solid red;
-      border-left: 2px solid red;
-      transform: skew(-0deg);
-      margin-top: 5%;
-      margin-left: 42%;
-      } 
-      </style>`;   
-      }
-  
-    else if(grade >= 91 && grade <= 179 ){
-      document.getElementById('textMargin_angles').innerHTML = "Obtuse angle";
-      document.getElementById('angle').innerHTML = 
-      `<style> 
-      #angle{
-      height: 70px; 
-      width: 100px;
-      border-bottom: 1px solid red;
-      border-left: 2px solid red;
-      transform: skew(40deg);
-      margin-top: 5%;
-      margin-left: 35%;
-      } 
-      </style>`;         
-     }
-  
-    else if(grade == 180 ){
-      document.getElementById('textMargin_angles').innerHTML = "Straight line";
-      document.getElementById('angle').innerHTML = 
-      `<style>
-      #angle {
-      height: 50px;
-      width: 180px;
-      border-bottom: 1px solid red;
-      margin-top: 5%;
-      margin-left: 20%;
-      }
-      </style>`;
-     }
+  var grade = Number(document.getElementById("result1").value);
+  var textMarginAngles = document.getElementById('textMargin_angles');
+  var angle = document.getElementById('angle');
 
-        else{
-           document.getElementById('textMargin_angles').innerHTML = " ";
-           document.getElementById('angle').innerHTML = " ";
-         } 
-    }
+  if (grade >= 1 && grade <= 89) {
+    textMarginAngles.innerHTML = "Acute angle";
+    angle.className = 'acute-angle';
+  } else if (grade === 90) {
+    textMarginAngles.innerHTML = "Right angle";
+    angle.className = 'right-angle';
+  } else if (grade >= 91 && grade <= 179) {
+    textMarginAngles.innerHTML = "Obtuse angle";
+    angle.className = 'obtuse-angle';
+  } else if (grade === 180) {
+    textMarginAngles.innerHTML = "Straight line";
+    angle.className = 'straight-line';
+  } else {
+    angle.className = 'angle';
+    textMarginAngles.innerHTML = "";
+    angle.innerHTML = "";
+  }
+}
